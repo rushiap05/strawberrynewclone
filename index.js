42d07d8b2ea7aa5aeca0584ec39f6f6dff4c3385
Yogesh  Sambare <bloggingscan@gmail.com>
2021-11-02 19:57:26 +0530

    updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
 // product script
