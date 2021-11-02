8f9cedf62fd2d7ef52ae9f892b665444c53ace1a
Aakash-Naykude <aakashnaykude123@gmail.com>
2021-11-02 16:26:09 +0530

    special page done

--- js elements/specials.js
+++ js elements/specials.js
@@ -24,12 +24,12 @@ function changeori(){
 //da32f09f6311aca662588aeda28758dfb5b2fec6feacf4d9c05d11876981ad1a
 //search?engine=google_product
 async function fetchd(){
-    let res = await fetch(`https://serpapi.com/search.json?q=beautyproduct&hl=en&gl=us&api_key=da32f09f6311aca662588aeda28758dfb5b2fec6feacf4d9c05d11876981ad1a`)
+    let res = await fetch(`https://serpapi.com/search.json?q=beauty product&hl=en&gl=us&api_key=da32f09f6311aca662588aeda28758dfb5b2fec6feacf4d9c05d11876981ad1a`)
     let data = await res.json()
     showprod(data.shopping_results)
     console.log(data)
 }
-fetchd()
+
 
 let cont1 = document.getElementById('cont1')
 cont1.style.display = "grid"
@@ -44,12 +44,13 @@ var c = 0;
 function showprod(d){
     cont1.innerHTML = null;
     d.forEach((prod)=>{
-        console.log(prod)
+        //console.log(prod)
         let div = document.createElement('div')
         div.style.borderRadius = "5px"
         var col = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
         div.style.backgroundColor = col
         div.style.height = "370px"
+        div.style.width = "300px"
         let head = document.createElement('p')
         head.textContent = prodhead[c]
         head.style.textAlign = "center"
@@ -69,7 +70,7 @@ function showprod(d){
         let tit = document.createElement('p')
         tit.innerText = prod.title
         tit.style.width = "100%"
-        tit.style.height = "80px"
+        //tit.style.height = "80px"
         tit.style.textAlign = "center"
         let pri = document.createElement('p')
         pri.innerText = prod.price
@@ -79,3 +80,120 @@ function showprod(d){
         cont1.append(div)
     })
 }
+
+
+
+async function fetchd2(){
+    let res = await fetch(`https://serpapi.com/search.json?q=naturalbeautyproduct&hl=en&gl=us&api_key=da32f09f6311aca662588aeda28758dfb5b2fec6feacf4d9c05d11876981ad1a`)
+    let data = await res.json()
+    showprod2(data.shopping_results)
+    //console.log(data)
+}
+fetchd2()
+let cont2 = document.getElementById('cont2')
+cont2.style.display = "grid"
+cont2.style.gridTemplateColumns = "auto auto auto auto"
+cont2.style.gap = "2%"
+var d = 0;
+function showprod2(d){
+    cont1.innerHTML = null;
+    d.forEach((prod)=>{
+        //console.log(prod)
+        let div = document.createElement('div')
+        div.style.borderRadius = "5px"
+        var col = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
+        div.style.backgroundColor = col
+        div.style.height = "370px"
+        div.style.width = "300px"
+        let head = document.createElement('p')
+        head.textContent = prodhead[d]
+        head.style.textAlign = "center"
+        head.style.fontSize = "large"
+        head.style.color = "white"
+        let img = document.createElement('img')
+        img.src = prod.thumbnail
+        img.style.backgroundColor = "white"
+        img.style.width = "97%"
+        img.style.height = "200px"
+        img.style.marginLeft = "1.5%"
+        let div1 = document.createElement('div')
+        div1.style.backgroundColor = "white"
+        div1.style.height = "120px"
+        div1.style.width ="97%"
+        div1.style.marginLeft = "1.5%"
+        let tit = document.createElement('p')
+        tit.innerText = prod.title
+        tit.style.width = "100%"
+        //tit.style.height = "80px"
+        tit.style.textAlign = "center"
+        let pri = document.createElement('p')
+        pri.innerText = prod.price
+        div1.append(tit, pri)
+        div.append(head, img, div1)
+        d++;
+        cont2.append(div)
+    })
+}
+
+async function fetchd3(){
+    let res = await fetch(`https://serpapi.com/search.json?q=make up gifts&hl=en&gl=us&api_key=da32f09f6311aca662588aeda28758dfb5b2fec6feacf4d9c05d11876981ad1a`)
+    let data = await res.json()
+    showprod3(data.shopping_results)
+    console.log(data)
+}
+fetchd3()
+
+let cont3 = document.getElementById('cont3')
+cont3.style.display = "grid"
+cont3.style.marginTop ="100px"
+cont3.style.gridTemplateColumns = "auto auto auto auto"
+cont3.style.gap = "2%"
+var prodhead = ["11.11 Days suprise", "Best Deals", "Best Sellers", "Early Bird 11.11 Sale", "Natural Beauty", "New Arrivals",
+                "11.11 Super Serums", "11.11 Beauty Guide", "11.11 One-Stop Budget Zone", "Holiday Giftsets Edition", "Holiday Hottest Offers",
+                "Holiday Edition Bath", "Special Purchase", "This Month's Top 40", "Point Reward Program", "Fragrance Specials", 
+                "New Customer Offer", "Download Our App","11.11 Super Serums", "11.11 Beauty Guide", "11.11 One-Stop Budget Zone",
+                "This Month's Top 40", "Point Reward Program", "Fragrance Specials"]
+var e = 0;
+function showprod3(d){
+    cont1.innerHTML = null;
+    d.forEach((prod)=>{
+        //console.log(prod)
+        let div = document.createElement('div')
+        div.style.borderRadius = "5px"
+        var col = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
+        div.style.backgroundColor = col
+        div.style.height = "370px"
+        div.style.width = "300px"
+        let head = document.createElement('p')
+        head.textContent = prodhead[e]
+        head.style.textAlign = "center"
+        head.style.fontSize = "large"
+        head.style.color = "white"
+        let img = document.createElement('img')
+        img.src = prod.thumbnail
+        img.style.backgroundColor = "white"
+        img.style.width = "97%"
+        img.style.height = "200px"
+        img.style.marginLeft = "1.5%"
+        let div1 = document.createElement('div')
+        div1.style.backgroundColor = "white"
+        div1.style.height = "120px"
+        div1.style.width ="97%"
+        div1.style.marginLeft = "1.5%"
+        let tit = document.createElement('p')
+        tit.innerText = prod.title
+        tit.style.width = "100%"
+        //tit.style.height = "80px"
+        tit.style.textAlign = "center"
+        let pri = document.createElement('p')
+        pri.innerText = prod.price
+        div1.append(tit, pri)
+        div.append(head, img, div1)
+        e++;
+        cont3.append(div)
+    })
+}
+
+
+
+fetchd()
