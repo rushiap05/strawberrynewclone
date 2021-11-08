e5fb75fb5d143df593434602a3b33b69b0bd38a5
Aakash-Naykude <aakashnaykude123@gmail.com>
2021-11-08 12:27:30 +0530

    import export

---
+++ header.js
@@ -0,0 +1,292 @@
+function header(){
+    return ` <!-- <!-header start-  --> 
+    <div id="header">
+          <div>
+              <img src="https://i.ibb.co/RzDqBPB/logo.png" alt="">
+          </div>
+          
+          <div id="headermiddle">
+            <div id="searchbar">
+              <input type="text" placeholder="SEARCH BRAND/PRODUCT" id="inputdata" >
+              <ion-icon name="search"></ion-icon>
+            </div>   
+            <div class="popularquery">
+              <button>Natural Beauty |</button>
+              <button>Estee Lauder |</button>
+              <button>Shiseido |</button>
+              <button>Double Serum</button>
+            </div>    
+          </div>
+          <div id="endheader">
+          </div>
+          
+      </div>
+  <!-- header end -->
+  
+  <!-- navbar starting From here -->
+  <div class="bg">
+  <!-- 1st -->
+  
+   <div class="nav-barCenter">
+      <div class="dropdown">
+          <div class="dropbtn"><span class="icon-span"><ion-icon name="reorder-four-outline"></ion-icon></span> SHOP BY BRAND</div>
+          <div class="dropdown-content">
+           <div class="row">
+          <div class="column">
+            <h3>face</h3>
+            <ul>Cleansers</ul>
+            <ul>Exfoliating & Peeling</ul>
+            <ul>Toners/ Face Mist</ul>
+            <ul>Serum & Concentrates</ul>
+            <ul>Moisturizers & Treatments</ul>
+        </div>
+            <div class="column">
+            <h3>face</h3>
+            <ul>Cleansers</ul>
+            <ul>Exfoliating & Peeling</ul>
+            <ul>Toners/ Face Mist</ul>
+            <ul>Serum & Concentrates</ul>
+            <ul>Moisturizers & Treatments</ul>
+        </div>
+          </div>
+          </div>
+        </div>
+        <!-- 2nd -->
+        <div class="dropdown">
+            <div class="dropbtn">SKINCARE</div>
+            <div class="dropdown-content">
+             <div class="row">
+            <div class="column">
+              <h3>FACE</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+          <div class="column">
+            <h3>EYE & LIP</h3>
+            <ul>Cleansers</ul>
+            <ul>Exfoliating & Peeling</ul>
+            <h3>SUN & CARE</h3>
+            <ul>Toners/ Face Mist</ul>
+            <ul>Serum & Concentrates</ul>
+            <ul>Moisturizers & Treatments</ul>
+        </div>
+              <div class="column">
+              <h3>BODY</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+            </div>
+            </div>
+          </div>
+        <!-- 3rd -->
+          <div class="dropdown">
+            <div class="dropbtn">MAKEUP</div>
+            <div class="dropdown-content">
+             <div class="row">
+            <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+              <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+          <div class="column">
+            <h3>face</h3>
+            <ul>Cleansers</ul>
+            <ul>Exfoliating & Peeling</ul>
+            <ul>Toners/ Face Mist</ul>
+            <ul>Serum & Concentrates</ul>
+            <ul>Moisturizers & Treatments</ul>
+        </div>
+        <div class="column">
+            <h3>face</h3>
+            <ul>Cleansers</ul>
+            <ul>Exfoliating & Peeling</ul>
+            <ul>Toners/ Face Mist</ul>
+            <ul>Serum & Concentrates</ul>
+            <ul>Moisturizers & Treatments</ul>
+        </div>
+            </div>
+            </div>
+          </div>
+        <!-- 4th -->
+          <div class="dropdown">
+            <div class="dropbtn">HAIRCARE</div>
+            <div class="dropdown-content">
+             <div class="row">
+            <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+              <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+            </div>
+            </div>
+          </div>
+        <!-- 5th -->
+          <div class="dropdown">
+            <div class="dropbtn">PERFUME</div>
+            <div class="dropdown-content">
+             <div class="row">
+            <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+              <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+            </div>
+            </div>
+          </div>
+        <!-- 6th -->
+          <div class="dropdown">
+            <div class="dropbtn">MEN'S SKINCARE</div>
+            <div class="dropdown-content">
+             <div class="row">
+            <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+              <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+            </div>
+            </div>
+          </div>
+        <!-- 7th -->
+          <div class="dropdown">
+            <div class="dropbtn">MEN'S COLOGNE</div>
+            <div class="dropdown-content">
+             <div class="row">
+            <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+              <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+            </div>
+            </div>
+          </div>
+        <!-- 8th -->
+          <div class="dropdown">
+            <div class="dropbtn">HOME SCENT</div>
+            <div class="dropdown-content">
+             <div class="row">
+            <div class="column">
+              <ul>face</ul>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+            </div>
+            </div>
+          </div>
+        <!-- 9th -->
+          <div class="dropdown">
+            <div class="dropbtn"><span class="icon-span"><ion-icon name="gift-outline"></ion-icon></span> SPECIAL</div>
+            <div class="dropdown-content">
+             <div class="row">
+            <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+              <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+            </div>
+            </div>
+          </div>
+          <!-- 10th -->
+          <div class="dropdown">
+            <div class="dropbtn"><span class="icon-span"><ion-icon name="star"></ion-icon></span> NEW</div>
+            <div class="dropdown-content">
+             <div class="row">
+            <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+              <div class="column">
+              <h3>face</h3>
+              <ul>Cleansers</ul>
+              <ul>Exfoliating & Peeling</ul>
+              <ul>Toners/ Face Mist</ul>
+              <ul>Serum & Concentrates</ul>
+              <ul>Moisturizers & Treatments</ul>
+          </div>
+            </div>
+            </div>
+          </div>
+  </div>
+  </div>
+  `
+}
+
+export { header };
